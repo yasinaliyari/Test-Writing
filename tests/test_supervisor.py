959aa0906a42cdc66c16e41cc53388b80798c958
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-29 04:44:40 +0330

    replace instance in setUp function

--- tests/test_supervisor.py
+++ tests/test_supervisor.py
@@ -5,18 +5,19 @@ from models import Supervisor
 class SupervisorTestCase(unittest.TestCase):
     def setUp(self):
         self.supervisor = Supervisor.login("yasin", "1234")
+        self.sample = Supervisor.sample()
 
     def test_all_data(self):
-        instance = Supervisor.sample()
         self.assertIsInstance(
-            instance, Supervisor, "sample does not return proper instance"
+            self.sample, Supervisor, "sample does not return proper instance"
         )
-        self.assertTrue(hasattr(instance, "username"), "instance")
-        self.assertTrue(hasattr(instance, "password"))
-        self.assertTrue(hasattr(instance, "phone_number"))
+        self.assertTrue(hasattr(self.sample, "username"), "instance")
+        self.assertTrue(hasattr(self.sample, "password"))
+        self.assertTrue(hasattr(self.sample, "phone_number"))
+        self.assertFalse(self.sample.logged_in, "Login is not False by default")
 
     def test_supervisor_protected_method(self):
-        self.assertIsNone(Supervisor.sample(), "Not raised on protected method")
+        self.assertIsNone(self.sample.protected(), "Not raised on protected method")
         self.assertListEqual(
             self.supervisor.protected(), [1, 2, 3], "Protected data dont match"
         )
